281ad3d33621a6857dcbc882799cb310ab94f827
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-06 17:40:02 +1000

    create a graph to work on

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var margin = { top: 10, right: 20, bottom: 30, left: 30 };
-var width = 400 - margin.left - margin.right;
+var margin = { top: 10, right: 20, bottom: 40, left: 30 };
+var width = 600 - margin.left - margin.right;
 var height = 600 - margin.top - margin.bottom;
 
 var svg = d3
@@ -7,4 +7,28 @@ var svg = d3
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
-  .attr("viewBox", `0 0 ${fullWidth} ${fullHeight}`);
+  .append("g")
+  .attr("transform", `translate( ${margin.left}, ${margin.top})`);
+
+svg
+  .append("rect")
+  .attr("width", width)
+  .attr("height", height)
+  .style("fill", "pink")
+  .style("stroke", "black");
+
+let yScale = d3.scaleLinear().domain([0, 100]).range([height, 0]);
+let yAxis = d3.axisLeft(yScale).ticks(10);
+svg.call(yAxis);
+
+let xScale = d3
+  .scaleTime()
+  .domain([new Date(2022, 0, 1, 6), new Date(2022, 0, 1, 9)])
+  .range([0, width]);
+let xAxis = d3
+  .axisBottom(xScale)
+  .ticks(5)
+  .tickSizeInner(10)
+  .tickSizeOuter(20)
+  .tickPadding(15);
+svg.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
